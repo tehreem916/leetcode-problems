28273f4a28358f01d019c835027f629c56cc1477
Tehreem Fatima <tehreemdar916@gmail.com>
2026-07-08 10:41:17 +0500

    Time: 0 ms (100%), Space: 9.9 MB (5.51%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int,int>freq;
+        int count=0;
+        for (int i=0;i<nums.size();i++) {
+            count+=freq[nums[i]];
+            freq[nums[i]]++;
+        }
+        return count;
+        
+    }
+};
