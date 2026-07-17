a2155e1906b36869ddfdaaa2dfa813b42f2dfba3
Tehreem Fatima <tehreemdar916@gmail.com>
2026-07-17 13:09:19 +0500

    Time: 3 ms (87.04%), Space: 127.8 MB (71.73%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> less, equal, greater;
+        int n = nums.size();
+        vector<int> result(n);
+
+        int left = 0;                  // next position for less
+        int right = n - 1;             // next position for greater
+        int pivotCount = 0;
+
+        // count pivots first
+        for (int num : nums)
+            if (num == pivot) pivotCount++;
+
+        // middle section boundaries
+        int mid = left;                // will be filled after less section
+        // we need to know where middle starts
+        // first count how many less elements
+        int lessCount = 0;
+        for (int num : nums)
+            if (num < pivot) lessCount++;
+
+        // fill result in one pass
+        int li = 0;               // index for less section
+        int mi = lessCount;       // index for equal section
+        int gi = lessCount + pivotCount;  // index for greater section
 
         for (int num : nums) {
-            if      (num < pivot)  less.push_back(num);
-            else if (num > pivot)  greater.push_back(num);
-            else                   equal.push_back(num);
+            if (num < pivot)       result[li++] = num;
+            else if (num == pivot) result[mi++] = num;
+            else                   result[gi++] = num;
         }
 
-        // merge all three
-        less.insert(less.end(), equal.begin(), equal.end());
-        less.insert(less.end(), greater.begin(), greater.end());
-
-        return less;
+        return result;
     }
 };
