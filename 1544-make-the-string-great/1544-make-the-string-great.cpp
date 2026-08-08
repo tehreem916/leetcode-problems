a6424028bdf6a874573f036da54710dbe1293fa7
Tehreem Fatima <tehreemdar916@gmail.com>
2026-08-08 22:19:10 +0500

    Time: 1 ms (23.97%), Space: 9 MB (13.62%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+            if (!st.empty() && abs(c - st.top()) == 32)
+                st.pop();    
+            else
+                st.push(c); 
+        }
+
+        string result = "";
+        while (!st.empty()) {
+            result = st.top() + result; 
+            st.pop();
+        }
+
+        return result;
+    }
+};
